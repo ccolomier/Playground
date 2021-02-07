892e90332cc934994134cfa52974b003bda2d579
Christopher Colomier <colomierac@gmail.com>
2021-02-06 22:52:01 -0500

    First run using the multiprocessing module. SUCCESS!

--- multiprocessing_trials.py
+++ multiprocessing_trials.py
@@ -4,3 +4,17 @@ Created on Fri Feb  5 21:30:06 2021
 
 @author: colomich
 """
+from multiprocessing import Pool
+import os
+
+def workerfunction(element):
+    #print(os.getpid())
+    return(element**2)
+
+long_list = range(30)
+
+if __name__ == '__main__':
+    with Pool(4) as p:
+        p.map(workerfunction, long_list)
+        
+    
